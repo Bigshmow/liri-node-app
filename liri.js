e74a45e4a1342b8db51653a444896af545b1183f
Bigshmow <dstewart88@gmail.com>
2019-08-26 16:34:54 -0700

    using axios to gather user input and search movie info on omdb

--- liri.js
+++ liri.js
@@ -1,5 +1,67 @@
 require("dotenv").config();
+var axios = require("axios");
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 
+function concertThis () {
+
+}
+
+function spotifyThis () {
+
+}
+
+function movieThis () {
+    var nodeArgs = process.argv;
+
+var movieName = "";
+
+for (var i = 3; i < nodeArgs.length; i++) {
+
+  if (i > 3 && i < nodeArgs.length) {
+    movieName = movieName + "+" + nodeArgs[i];
+  } else {
+    movieName += nodeArgs[i];
+
+  }
+}
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+// console.log(queryUrl);
+
+axios.get(queryUrl).then(
+  function(response) {
+    console.log("Title: " + response.data.Title + "\nRelease Date: " + response.data.Year + "\nIMDB Rating: " + response.data.Ratings[0].Value + "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\nThis movie was produced in: " + response.data.Country + "\nLanguages: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nMain Actors: " + response.data.Actors);
+  })
+  .catch(function(error) {
+    if (error.response) {
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
+
+}
+
+function dowhatitSays () {
+
+}
+
+function liriBot () {
+    if (process.argv[2] === "movie-this") {
+        // console.log("movies");
+        movieThis();
+    }
+};
+
+liriBot();
